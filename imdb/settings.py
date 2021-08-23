6097f0ee8caae11818d6d5381a91d5c4c2cd545d
shehsawarali <shehsawar.ali@arbisoft.com>
2021-08-23 20:32:35 +0500

    added local sql connection

--- imdb/settings.py
+++ imdb/settings.py
@@ -74,7 +74,15 @@ WSGI_APPLICATION = "imdb.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "imdb",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
+    }
 }
 
 
