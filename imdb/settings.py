a76e733d6da2d0f6d649114b1f6ff2a3e5307d2d
shehsawarali <shehsawar.ali@arbisoft.com>
2021-09-07 19:13:56 +0500

    Fix PR #4 issues

--- imdb/settings.py
+++ imdb/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "django_countries",
+    "django_inlinecss",
     "users",
     "core",
     "tsv",
@@ -158,7 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
