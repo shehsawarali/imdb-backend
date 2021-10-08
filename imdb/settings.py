5cf0c3e193ef4191be7b41c0c12d2c21d69b110e
shehsawarali <shehsawar.ali@arbisoft.com>
2021-10-08 18:05:08 +0500

    set DEBUG = True

--- imdb/settings.py
+++ imdb/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["filmfilia-server.herokuapp.com", "localhost"]
 
